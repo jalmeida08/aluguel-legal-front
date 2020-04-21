5194fb65bb3d81ec723d077ca6b5eec40f23b58e
Jefferson <jefferson08jsa@hotmail.com7>
2020-04-21 14:57:32 -0300

    taslterando o interceptor

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,7 +8,7 @@ import { AppComponent } from './app.component';
 import { LoginModule } from './login/login.module';
 import { HttpClientModule } from '@angular/common/http';
 import { HomeModule } from './home/home.module';
-import { Interceptor } from './config/Interceptor.interceptor';
+import { Interceptor } from './_config/Interceptor.interceptor';
 import { DataService } from './services/data.service';
 import { MenuComponent } from './menu/menu.component';
 import { AdministracaoModule } from './administracao/administracao.module';
